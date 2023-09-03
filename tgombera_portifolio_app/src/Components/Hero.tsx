c02cea1862d16fe002d755c09610abda6d156507
TauG <tauraigombera@gmail.com>
2023-09-03 06:21:13 +0200

    changed hero text

--- tgombera_portifolio_app/src/Components/Hero.tsx
+++ tgombera_portifolio_app/src/Components/Hero.tsx
@@ -3,9 +3,9 @@ function Hero() {
     <section className="container mt-16 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
       <div className="flex flex-wrap md:flex-nowrap">
         <div className="flex flex-wrap justify-center md:justify-start max-w-xl mt-0 md:my-36">
-          <h1 className="font-bold text-5xl md:text-6xl lg:text-7xl text-center md:text-left">
-            Exceptional
-            <br /> solutions with .NET/C#.
+          <h1 className="font-bold text-5xl text-center md:text-left">
+            Exceptional Software
+            <br /> Solutions with <span className="text-theme"> .NET </span>
           </h1>
           <div className="w-full flex justify-center md:justify-start">
             <button className="px-8 py-4 bg-theme text-white font-bold mt-12 flex items-center space-x-3">
