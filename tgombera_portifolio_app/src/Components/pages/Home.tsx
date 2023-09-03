1c817f63310c6d1f64f66e194e3ff69b6f040592
TauG <tauraigombera@gmail.com>
2023-09-03 06:55:29 +0200

    added skills section

--- tgombera_portifolio_app/src/Components/pages/Home.tsx
+++ tgombera_portifolio_app/src/Components/pages/Home.tsx
@@ -1,14 +1,14 @@
 import Navbar from "../Navbar";
 import Hero from "../Hero";
-import About from "./About";
 import Projects from "./Projects";
 import Hire from "./Hire";
+import Skills from "../Skills";
 function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <About />
+      <Skills />
       <Projects />
       <Hire />
     </div>
