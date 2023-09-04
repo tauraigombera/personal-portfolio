4ce36b52a52e19fe72126e2debce8731a1051982
TauG <tauraigombera@gmail.com>
2023-09-04 09:30:10 +0200

    modified hero section

--- portifolio-app/src/components/Hero.tsx
+++ portifolio-app/src/components/Hero.tsx
@@ -1,13 +1,13 @@
 function Hero() {
   return (
-    <section className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+    <section className="container flex justify-between items-center mx-auto px-8 mt-16 md:px-14 lg:px-24 w-full">
       <div className="flex flex-wrap md:flex-nowrap">
         <div className="flex flex-wrap justify-center md:justify-start max-w-xl mt-0 md:my-36">
           <h1 className="font-bold text-5xl text-center md:text-left">
             Exceptional Software
             <br /> Solutions with <span className="text-theme"> .NET </span>
           </h1>
-          <p className="mt-12">
+          <p className="mt-12 text-center md:text-left">
             Hi, I'm Taurai Gombera, a .NET Software Developer based in Blantyre,
             Malawi. I craft digital solutions that streamline processes and
             enhance user experiences.
