17247baa7d1b1299e9c14ccced3d44e35998d159
TauG <tauraigombera@gmail.com>
2024-03-04 17:22:30 +0200

    added icons folder

---
+++ portifolio-app/src/components/icons/Placeholder.tsx
@@ -0,0 +1,11 @@
+const Placeholder = () => {
+  return (
+    <img
+      src="https://via.placeholder.com/400x200"
+      alt="News Image"
+      className="w-full h-40 object-cover"
+    />
+  );
+};
+
+export default Placeholder;
