9d1d4720ce76a41047f07cd08333018b7a774a76
TauG <tauraigombera@gmail.com>
2024-03-04 14:50:14 +0200

    added blog page

---
+++ portifolio-app/src/components/pages/Blog.tsx
@@ -0,0 +1,13 @@
+function Connect() {
+  return (
+    <section className="container mx-auto min-h-scree px-8 md:px-14 lg:px-24 w-full">
+      <h2 className="flex justify-center text-3xl font-semibold mb-8 pt-64">
+        Blog Articles
+      </h2>
+      <p className="flex justify-center mb-8">Read my articles</p>
+      <div className=""></div>
+    </section>
+  );
+}
+
+export default Connect;
